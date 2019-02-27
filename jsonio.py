ddaeeb0a79eda80ea8fcab70fd09f8a85b6a18b2
intern-hamada <t_hamada@timedia.co.jp>
2019-02-27 19:30:53 +0900

    :tada: implements the command to get the html source at issue page

--- jsonio.py
+++ jsonio.py
@@ -1,9 +1,8 @@
 import json
 import os.path
 
-json_path = os.path.join('.', 'json')
-config_path = os.path.join(json_path, 'config.json')
-status_path = os.path.join(json_path, 'state.json')
+JSON_PATH = os.path.join('.', 'json')
+STATUS_PATH = os.path.join(JSON_PATH, 'state.json')
 
 def write_json(data, path):
     json.dump(
--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-from jsonio import read_json, write_json, status_path
+from jsonio import read_json, write_json, STATUS_PATH
 from setting import LoginData
 
-url = "https://etrack.timedia.co.jp/EasyTracker/"
+BASE_URL = "https://etrack.timedia.co.jp/EasyTracker/"
 session = requests.session()
 
-auth_invalid_message = "ログインID、メールアドレスもしくはパスワードが間違っています"
+AUTH_INVALID_MESSAGE = "ログインID、メールアドレスもしくはパスワードが間違っています"
 
 def login(user_id, password):
-    path = "Login.aspx"
+    PATH = "Login.aspx"
 
-    status = read_json(status_path)
+    status = read_json(STATUS_PATH)
     params = {
         '__VIEWSTATE': status["paramators"]["login"]["__VIEWSTATE"],
         '__VIEWSTATEGENERATOR': status["paramators"]["login"]["__VIEWSTATEGENERATOR"],
@@ -23,20 +23,29 @@ def login(user_id, password):
         'buttonLogin': 'ログイン'
     }
 
-    res = session.post(url+path, data=params)
-    if res.text.find(auth_invalid_message) > -1:
-        sys.exit(auth_invalid_message)
+    res = session.post(BASE_URL+PATH, data=params)
+    if res.text.find(AUTH_INVALID_MESSAGE) > -1:
+        sys.exit(AUTH_INVALID_MESSAGE)
 
-    c = session.cookies.get("ASP.NET_SessionId")
-    session_id = {"ASP.NET_SessionId": c}
+    cookie = session.cookies.get("ASP.NET_SessionId")
+    session_id = {"ASP.NET_SessionId": f"{cookie}"}
     status["session"] = session_id
 
     status["login"]["userid"] = user_id
     status["login"]["password"] = password
 
-    write_json(status, status_path)
+    write_json(status, STATUS_PATH)
 
-    print("ログインしました ID: {}".format(user_id))
+    print(f"ログインしました ID: {user_id}")
+
+def get_html_with_session(path):
+    status = read_json(STATUS_PATH)
+    cookies = status["session"]
+
+    res = session.get(BASE_URL+path, cookies=cookies)
+    if res.text.find(AUTH_INVALID_MESSAGE) > -1:
+        sys.exit(AUTH_INVALID_MESSAGE)
+    return res.text
 
 def genarate_project_info(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -44,24 +53,49 @@ def genarate_project_info(html):
     TABLE_ID = "_ctl0_ContentPlaceHolder1_gridList"
     table = soup.find(attrs={"id": TABLE_ID})
     rows = table.find_all("tr")[1:]
-    project_info = [list(map(lambda td: td.text, row.find_all("td"))) for row in rows]
+    project_info = [list(map(lambda td: td.text, row.find_all("td")[0:2])) for row in rows]
 
     return project_info
 
 def list_project():
-    path = "main/MyPage.aspx"
+    PATH = "main/MyPage.aspx"
 
-    status = read_json(status_path)
-    header = status["session"]
-    
-    res = session.get(url+path, headers=header)
-    html = res.text
+    html = get_html_with_session(PATH)
     project_info = genarate_project_info(html)
 
-    for (group, name, _, _, _) in project_info:
-        print("{}@{}".format(name, group))
+    for (group, name) in project_info:
+        print(f"{name}@{group}")
+
+def get_selected_project_path(html, name):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    TABLE_ID = "_ctl0_ContentPlaceHolder1_gridList"
+    table = soup.find(attrs={"id": TABLE_ID})
+    rows = table.find_all("tr")[1:]
+
+    name_and_path = {row.find_all("td")[1].text: row.find_all("td")[1].a.get("href")[3:] for row in rows}
+    return name_and_path[name]
+
+def select_project(name):
+    MYPAGE_PATH = "main/MyPage.aspx"
+    mypage_html = get_html_with_session(MYPAGE_PATH)
+
+    project_path = get_selected_project_path(mypage_html, name)
+    project_html = get_html_with_session(project_path)
+
+    print(project_html)
 
 if __name__=="__main__":
-    ld = LoginData()
-    login(ld.user_id, ld.password)
-    list_project()
+    # コマンドライン引数でtry-exceptするべき
+    command = "--select" # sys.argv[1]
+
+    # 引数のvalidationをしてから実行？
+    if command == "--login":
+        ld = LoginData()
+        userid, password = ld.user_id, ld.password # DEBUG
+        login(userid, password)
+    elif command == "--list":
+        list_project()
+    elif command == "--select":
+        name = "自動化テスト" # DEBUG
+        select_project(name)
