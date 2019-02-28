3a6499c9d03f0f0979842911c260d10107abad8a
intern-hamada <t_hamada@timedia.co.jp>
2019-02-28 12:34:09 +0900

    :+1: implements --select command

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def login(user_id, password):
 
     print(f"ログインしました ID: {user_id}")
 
-def get_html_with_session(path):
+def __generate_html_with_session(path):
     status = read_json(STATUS_PATH)
     cookies = status["session"]
 
@@ -42,26 +42,26 @@ def get_html_with_session(path):
         sys.exit(AUTH_INVALID_MESSAGE)
     return res.text
 
-def genarate_project_info(html):
+def __genarate_project_info(html):
     soup = BeautifulSoup(html, 'html.parser')
 
     TABLE_ID = "_ctl0_ContentPlaceHolder1_gridList"
     table = soup.find(attrs={"id": TABLE_ID})
     rows = table.find_all("tr")[1:]
-    project_info = [list(map(lambda td: td.text, row.find_all("td")[0:2])) for row in rows]
+    project_info = [list(map(lambda td: td.text, row.find_all("td")[:2])) for row in rows]
 
     return project_info
 
-def list_project():
+def list_projects():
     PATH = "main/MyPage.aspx"
 
-    html = get_html_with_session(PATH)
-    project_info = genarate_project_info(html)
+    html = __generate_html_with_session(PATH)
+    project_info = __genarate_project_info(html)
 
     for (group, name) in project_info:
         print(f"{name}@{group}")
 
-def get_selected_project_path(html, name):
+def __generate_selected_project_path(html, name):
     soup = BeautifulSoup(html, 'html.parser')
 
     TABLE_ID = "_ctl0_ContentPlaceHolder1_gridList"
@@ -72,16 +72,35 @@ def get_selected_project_path(html, name):
         project_name = row.find_all("td")[1] 
         if project_name.text == name:
             return project_name.a.get("href")[3:]
+    
+    sys.exit("selected project does not exist")
+
+def __genarate_issues(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    TABLE_ID = "_ctl0_ContentPlaceHolder1_gridList"
+    table = soup.find(attrs={"id": TABLE_ID})
+    rows = table.find_all("tr")[2:-1]
+    issues = [list(map(lambda td: td.text, row.find_all("td"))) for row in rows]
+
+    return issues
+
 
 def select_project(name):
     MYPAGE_PATH = "main/MyPage.aspx"
-    mypage_html = get_html_with_session(MYPAGE_PATH)
+    mypage_html = __generate_html_with_session(MYPAGE_PATH)
+    project_path = __generate_selected_project_path(mypage_html, name)
 
-    project_path = get_selected_project_path(mypage_html, name)
-    project_html = get_html_with_session(project_path)
+    status = read_json(STATUS_PATH)
+    status["paramators"]["select"]["name"] = name
+    status["paramators"]["select"]["path"] = project_path
+    write_json(status, STATUS_PATH)
 
-    #soup = BeautifulSoup(project_html, 'html.parser')
+    project_html = __generate_html_with_session(project_path)
+    issues = __genarate_issues(project_html)
 
+    for no, name, status_, priority, type_, category, asign in issues:
+        print(f"No. {no}: {name}\nステータス: {status_}, 重要度: {priority}, タイプ: {type_}, アサイン: {asign}")
 
 if __name__=="__main__":
     # test
@@ -94,7 +113,7 @@ if __name__=="__main__":
         userid, password = login_data["user_id"], login_data["password"] # DEBUG
         login(userid, password)
     elif command == "--list":
-        list_project()
+        list_projects()
     elif command == "--select":
         name = "自動化テスト" # DEBUG
         select_project(name)
