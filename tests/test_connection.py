cfffd9a04c56e9bfcc0e9e1f7847454e1dda022f
intern-hamada <t_hamada@timedia.co.jp>
2019-03-05 19:14:22 +0900

    :+1: create test module

---
+++ tests/test_connection.py
@@ -0,0 +1,10 @@
+import unittest
+from src.connection import (
+    check_request_success,
+    get_with_session,
+    post_with_session,
+    authenticate
+)
+
+class ConnectionTest(unittest.TestCase):
+    pass
---
+++ tests/test_core.py
@@ -0,0 +1,13 @@
+import unittest
+from src.core import (
+    setup,
+    login,
+    list_projects,
+    select_project,
+    list_issues,
+    post_issue
+)
+
+
+class CoreTest(unittest.TestCase):
+    pass
